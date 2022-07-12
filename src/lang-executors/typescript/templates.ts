a1497924ffd64262b3e1107c928fab4d9920d044
Lucas Avila <lucaxx@gmail.com>
2022-07-12 20:12:58 -0300

    fix build bugs

--- src/lang-executors/typescript/templates.ts
+++ src/lang-executors/typescript/templates.ts
@@ -1,3 +1,4 @@
+const codeTemplateStr = `
 import * as util from "util";
 
 type BlockExecutionResult = {
@@ -97,3 +98,38 @@ export default async function main(): Promise<Ret[]> {
     }
     return __arr;
 }
+
+`;
+
+const indexTemplateStr = `
+{{##0##}}
+
+
+type GenDef = {
+    generator: () => Promise<any>;
+    source: string;
+};
+
+const main = async () => {
+    const response: Record<string, any> = {};
+    const results = await Promise.all(generators.map(it => it.generator()))
+    generators.forEach((it, idx) => {
+        response[it.source] = results[idx]
+    })
+    process.stdout.write("\\n##eval-md-start##\\n");
+    process.stdout.write(JSON.stringify(response));
+    process.stdout.write("\\n##eval-md-end##\\n");
+};
+main().catch((e) => {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    process.exit(1);
+});
+
+`;
+
+const applyTemplate1 = (template: string) => (replacement: string) =>
+    template.replace("{{##0##}}", replacement);
+
+export const codeTemplate = applyTemplate1(codeTemplateStr);
+export const indexTemplate = applyTemplate1(indexTemplateStr);
